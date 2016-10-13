3b78a9064f58828ea36bf6dda5dac1bc49f81a4b
changsheng <changsheng@51kik.com>
2016-10-13 19:24:00 +0800

    容易题

---
+++ src/com/leo/lintcode/ReplaceBlank.java
@@ -0,0 +1,43 @@
+package com.leo.lintcode;
+
+
+/**
+ * Created by ChangSheng on 2016/10/13 16:09.
+ */
+public class ReplaceBlank {
+
+    //做替换一般使用倒序,然后用两个指针来确定位置
+    public int replaceBlank(char[] string, int length) {
+        int pOld = 0;
+        // Write your code here
+        int blank = 0;
+        for (int i = 0 ; i < length ; i++){
+            ++pOld;
+            if (string[i] == ' '){
+                blank ++;
+            }
+        }
+        int pNew = length + blank * 2;
+        int result = length + blank * 2;
+        while (pOld>=0 && pNew > pOld){
+            if (string[pOld] == ' '){
+                string[pNew--] = '0';
+                string[pNew--] = '2';
+                string[pNew--] = '%';
+            }else{
+                string[pNew--] = string[pOld];
+            }
+            --pOld;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        char[] a = new char[10000];
+        String s = " hello world ";
+        for (int i = 0; i<s.length(); i++){
+            a[i] = s.toCharArray()[i];
+        }
+        System.out.print(new ReplaceBlank().replaceBlank(a,13));
+    }
+}
